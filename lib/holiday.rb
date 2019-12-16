4ed7392c7017032e59aa5c97d7acb905fbcfe3be
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-16 04:43:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  r  result =[]
+  result =[]
   holiday_hash.each { |season,holiday| holiday.values.each{|item|if item.include?("BBQ\n")
   result.push(holiday)
   end}}
